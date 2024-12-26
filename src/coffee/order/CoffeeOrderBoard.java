b31d1697e7447b59109c0837b0c145a1d02e8238
evastanishevska <belkinxvosteg@gmail.com>
2024-12-26 13:22:46 +0100

    add

---
+++ src/coffee/order/CoffeeOrderBoard.java
@@ -0,0 +1,45 @@
+package coffee.order;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CoffeeOrderBoard {
+    private final List<Order> orders;
+    private int nextNumber;
+
+    public CoffeeOrderBoard() {
+        this.orders = new ArrayList<>();
+        this.nextNumber = 1;
+    }
+
+    public void addNewOrder(String name) {
+        Order order = new Order(name, nextNumber);
+        orders.add(order);
+        nextNumber++;
+    }
+
+    public Order deliver() {
+        if (!orders.isEmpty()) {
+            return orders.removeFirst();
+        }
+        return null;
+    }
+
+    public Order deliver(int number) {
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.get(i).getNumber() == number) {
+                return orders.remove(i);
+            }
+        }
+        return null;
+    }
+
+    public void draw() {
+        System.out.println("                 ");
+        System.out.println("Number | Name");
+        for (Order order : orders) {
+            System.out.println(order);
+        }
+    }
+}
+
