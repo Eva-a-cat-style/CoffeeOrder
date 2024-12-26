e751e3ddc4443683b18620fb13b931cee7135788
evastanishevska <belkinxvosteg@gmail.com>
2024-12-26 13:24:44 +0100

    rem

--- src/coffee/order/Order.java
+++ src/coffee/order/Order.java
@@ -1,21 +1,2 @@
-package coffee.order;
 
-public class Order {
-    private final String name;
-    private final int number;
-
-    public Order(String name, int number) {
-        this.name = name;
-        this.number = number;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public String toString() {
-        return number + " | " + name;
-    }
-}
 
